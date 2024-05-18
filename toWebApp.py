89baf4d711bd2456f836de0d21c44559615c2abb
minhthong3 <63642154+minhthong3@users.noreply.github.com>
2024-05-18 16:41:35 +0700

    Update toWebApp.py

--- toWebApp.py
+++ toWebApp.py
@@ -8,22 +8,6 @@ import os
 # Đường dẫn đến tệp credentials.json
 credentials_path = "C:\\Users\\thong\\Downloads\\vnw.data\\datavnwealth-25a353ea3781.json"
 
-# Phạm vi truy cập của Google Sheets
-scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-
-# Thông tin xác thực API
-credentials = Credentials.from_service_account_file(credentials_path, scopes=scope)
-
-# Kết nối đến Google Sheets
-client = gspread.authorize(credentials)
-
-import streamlit as st
-
-
-# XÁC THỰC TRUY CẬP VÀO GOOGLE SHEET
-# Đường dẫn đến tệp credentials.json
-credentials_path = "C:\\Users\\thong\\Downloads\\vnw.data\\datavnwealth-25a353ea3781.json"
-
 # Thiết lập xác thực và kết nối với Google Sheets
 scope = ["https://spreadsheets.google.com/feeds", 
          "https://www.googleapis.com/auth/spreadsheets", 
@@ -35,9 +19,6 @@ credentials = Credentials.from_service_account_file(credentials_path, scopes=sco
 
 # Kết nối đến Google Sheets
 client = gspread.authorize(credentials)
-
-# Mở Google Sheets bằng URL hoặc tên
-sheet = client.open("suggestion_table").sheet1
 
 # Lấy tất cả dữ liệu từ sheet
 data = sheet.get_all_records()
